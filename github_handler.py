b5ae82d5cb0a63f4a9bd1b4b8991714556dd47aa
gietema <jochem@giete.ma>
2020-06-13 22:15:57 +0100

    Create github handler

---
+++ github_handler.py
@@ -0,0 +1,60 @@
+"""Class that can fetch content of a file and push new commit with content"""
+import base64
+import os
+import json
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class GitHubHandler:
+    def __init__(self, input_filename: str, repository: str, branch: str):
+        self.input_filename = input_filename
+        self.repository = repository
+        self.branch = branch
+        self.url = (
+            f"https://api.github.com/repos/{repository}/contents/{input_filename}"
+        )
+        self.header, self.content, self.sha = None, None, None
+        self.base64content = None
+
+    def fetch_content(self) -> str:
+        """Get content of input filename"""
+        token = os.getenv("GITHUB_ACCESS_TOKEN")
+        base64content = base64.b64encode(open(self.input_filename, "rb").read())
+        self.base64content = base64content
+        data = requests.get(
+            f"{self.url}?ref={self.branch}", headers={"Authorization": f"token {token}"}
+        ).json()
+        self.sha = data["sha"]
+
+        return base64.b64decode(data["content"]).decode("utf-8")
+
+    def push(self, content: str):
+        """
+        Commits and pushes header + content to GitHub
+        """
+        # necessary to create valid message
+        content = base64.encodebytes(content.encode("utf-8")).decode("utf-8")
+
+        # create message
+        message = json.dumps(
+            {
+                "message": "Update bookmarks",
+                "branch": self.branch,
+                "content": content,
+                "sha": self.sha,
+            }
+        )
+
+        # push content to github
+        print("Pushing content to GitHub..")
+        response = requests.put(
+            self.url,
+            data=message,
+            headers={
+                "Content-Type": "application/json",
+                "Authorization": "token " + os.getenv("GITHUB_ACCESS_TOKEN"),
+            },
+        )
